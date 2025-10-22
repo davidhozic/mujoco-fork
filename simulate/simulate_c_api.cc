2cdc243acc8a170f4684e09f04e9c24f5a3409a2
David Hozic <davidhozic@gmail.com>
2025-10-23 01:11:08 +0200

    C API for windows

--- simulate/simulate_c_api.cc
+++ simulate/simulate_c_api.cc
@@ -1,14 +1,18 @@
 #include "simulate_c_api.h"
+#include <stdint.h>
 
 extern "C" {
-    Sim* mujoco_Simulate_create(mjvCamera* cam, mjvOption* opt, mjvPerturb* pert, mjvScene* user_scn, bool is_passive) {
+    Sim* mujoco_cSimulate_create(mjvCamera* cam, mjvOption* opt, mjvPerturb* pert, mjvScene* user_scn, bool is_passive) {
         return new Sim(cam, opt, pert, user_scn, is_passive);
     }
-    void mujoco_Simulate_destroy(Sim* sim) {
+    void mujoco_cSimulate_destroy(Sim* sim) {
         delete sim;
     }
-    void mujoco_Simulate_RenderInit(Sim* sim) { sim->RenderInit(); }
-    void mujoco_Simulate_RenderStep(Sim* sim, bool update_timer) { sim->RenderStep(update_timer); }
-    void mujoco_Simulate_RenderCleanup(Sim* sim) { sim->RenderCleanup(); }
-    void mujoco_Simulate_Sync(Sim* sim, bool stateonly) { sim->Sync(stateonly); }
+    void mujoco_cSimulate_RenderInit(Sim* sim) { sim->RenderInit(); }
+    uint8_t mujoco_cSimulate_RenderStep(Sim* sim, bool update_timer) { return (uint8_t) sim->RenderStep(update_timer); }
+    void mujoco_cSimulate_RenderCleanup(Sim* sim) { sim->RenderCleanup(); }
+    void mujoco_cSimulate_Sync(Sim* sim, bool stateonly) { sim->Sync(stateonly); }
+    void mujoco_cSimulate_Load(Sim* sim, mjModel* m, mjData* d, const char* displayed_filename) {
+        sim->Load(m, d, displayed_filename);
+    }
 }
--- simulate/simulate_c_api.h
+++ simulate/simulate_c_api.h
@@ -1,10 +1,11 @@
 #include "simulate.h"
 extern "C" {
     using Sim = mujoco::Simulate;
-    mujoco::Simulate* mujoco_Simulate_create(mjvCamera* cam, mjvOption* opt, mjvPerturb* pert, mjvScene* user_scn, bool is_passive);
-    void mujoco_Simulate_destroy(Sim* sim);
-    void mujoco_Simulate_RenderInit(Sim* sim);
-    void mujoco_Simulate_RenderStep(Sim* sim, bool update_timer);
-    void mujoco_Simulate_RenderCleanup(Sim* sim);
-    void mujoco_Simulate_Sync(Sim* sim, bool stateonly);
+    mujoco::Simulate* mujoco_cSimulate_create(mjvCamera* cam, mjvOption* opt, mjvPerturb* pert, mjvScene* user_scn, bool is_passive);
+    void mujoco_cSimulate_destroy(Sim* sim);
+    void mujoco_cSimulate_RenderInit(Sim* sim);
+    uint8_t mujoco_cSimulate_RenderStep(Sim* sim, bool update_timer);
+    void mujoco_cSimulate_RenderCleanup(Sim* sim);
+    void mujoco_cSimulate_Sync(Sim* sim, bool stateonly);
+    void mujoco_cSimulate_Load(Sim* sim, mjModel* m, mjData* d, const char* displayed_filename);
 }
